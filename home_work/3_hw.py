8834511dc2db2ce1fe30a1ce6b4ae3f707002e12
bordovaya <bordovaya@mail.ru>
2023-09-06 14:43:27 +0300

    Second commit

---
+++ home_work/3_hw.py
@@ -0,0 +1,40 @@
+x = 1
+y = 2
+if x > y:
+    print(x)
+else:
+    print(y)
+
+
+a = 0
+b = 135
+if abs(a - b) == 135:
+    print('Yes')
+else:
+    print('No')
+
+
+c = 9
+if c in (1, 2) or c == 12:
+    print('Зима')
+elif c in (3, 4, 5):
+    print('Весна')
+elif c in (6, 7, 8):
+    print('Лето')
+elif c in (9, 10, 11):
+    print('Осень')
+
+
+d = 11
+e = 20
+f = 30
+if d>10 and e>10 and f >10:
+    print('Yes')
+else:
+    print('No')
+
+
+
+
+
+
